ffe1970a051c4050e684eea4dbab51355516bcf6
Mihir Mandavia <mihirmandavia7@gmail.com>
2025-03-13 23:56:06 +0530

    added tasks
    added tasks to each users and made a few ui changes

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,6 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { type User } from './user.model';
+
 @Component({
   selector: 'app-user',
   standalone: true,
@@ -7,15 +9,14 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
   styleUrl: './user.component.css',
 })
 export class UserComponent {
-  @Input({required:true}) id!:string;
-  @Input({ required: true }) avatar!: string;
-  @Input({ required: true }) name!: string;
-  @Output() select = new EventEmitter();
+  @Input({ required: true }) user!: User;
+  @Input({ required: true }) selected!: boolean;
+  @Output()
+  select = new EventEmitter();
   get imagePath() {
-    return 'assets/users/' + this.avatar;
+    return 'assets/users/' + this.user.avatar;
   }
-  onSelectUser() 
-  {
-  this.select.emit(this.id);
+  onSelectUser() {
+    this.select.emit(this.user.id);
   }
 }
